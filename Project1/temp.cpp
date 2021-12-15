a4d79649dee2dd125a387045d4b8f31b24073276
Dohyeon Ryu <feg0823@naver.com>
2021-12-15 15:41:40 +0900

    .

--- Project1/temp.cpp
+++ Project1/temp.cpp
@@ -41,6 +41,7 @@ int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
     
     int num = 0;
+    int new_num = 0;
     cin >> n >> m;
     for (int i = 0; i < n; i++) {
         string str;
